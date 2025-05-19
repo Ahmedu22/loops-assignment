24b64fc7c6420537de509e05a917793a831d6c28
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-19 12:33:38 +0300

    loops-assignment

---
+++ apps.js
@@ -0,0 +1,22 @@
+// for loop
+
+for(let i = 1; i <= 10; i++){
+    console.log("Ahmed ", i);
+}
+
+// while loop
+
+let a = 10;
+while(a <= 100){
+    console.log("Ahmed", a);
+    a++;
+}
+
+// do while
+
+let y = 20;
+
+do{
+    console.log("Ahmed",y);
+    y++;
+}while (y <= 400);
